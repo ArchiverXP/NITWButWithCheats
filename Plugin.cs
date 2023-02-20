37183a620280dc78daa1f2164fb9a0186ba974a6
ArchiverXP <108936256+ArchiverXP@users.noreply.github.com>
2023-02-20 02:26:35 -0500

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.Reflection;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace MoonJump
+{
+    [BepInPlugin("com.archiverxp.nitwcheats", PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {"com.archiverxp.nitwcheats"} is loaded!");
+            SceneManager.LoadScene("BusStation");
+            var harmony = new Harmony("com.archiverxp.nitwcheats");
+            Harmony.DEBUG = true;
+            var assembly = Assembly.GetExecutingAssembly(); //copy and pasted from docs
+            harmony.PatchAll(assembly);
+        }
+    }
+}
